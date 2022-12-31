c795ba42edfdc00ed1df5c0a6a0e2e2c7e8f9093
Raphael Meyer <raphael.akira.meyer@gmail.com>
2022-12-31 16:55:42 +0100

    Day 24 - Optimization 🚀

--- src/Day24/BlizzardBasin.hs
+++ src/Day24/BlizzardBasin.hs
@@ -2,11 +2,11 @@ module Day24.BlizzardBasin where
 
 import qualified AoC.Puzzle as Puzzle
 import qualified Control.Monad.State as S
-import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 import Data.Tuple.Extra ((&&&))
+import qualified Data.Vector as Vector
 
 solver :: Puzzle.Solver
 solver = Puzzle.Solver 24 "❄️ Blizzard Basin" solve
@@ -14,13 +14,11 @@ solver = Puzzle.Solver 24 "❄️ Blizzard Basin" solve
 solve :: String -> Puzzle.Solution
 solve = (partOne &&& partTwo) . parseInput . Text.lines . Text.pack
 
-data Direction = MoveUp | MoveDown | MoveLeft | MoveRight deriving (Eq, Show)
+data Blizzard = None | MoveUp | MoveDown | MoveLeft | MoveRight deriving (Eq, Show)
 
 type Pos = (Int, Int)
 
-type Blizzard = (Pos, Direction)
-
-type Blizzards = [Blizzard]
+type Blizzards = Vector.Vector (Vector.Vector Blizzard)
 
 data State = State {blizzards :: Blizzards, positions :: Set.Set Pos, time :: Int, start :: Pos, goal :: Pos, width :: Int, height :: Int} deriving (Eq, Show)
 
@@ -44,15 +42,11 @@ goForSnack s = S.evalState takeStep $ back {positions = Set.singleton (start s),
 takeStep :: S.State State Int
 takeStep = do
   updateTime
-  updateBlizzards
   updatePositions
   s <- S.get
   if goal s `elem` positions s
     then return (time s)
     else takeStep
-
-updateBlizzards :: S.State State ()
-updateBlizzards = S.modify (\s -> s {blizzards = map (updateBlizzard (width s) (height s)) (blizzards s)})
 
 updatePositions :: S.State State ()
 updatePositions = S.modify (\s -> s {positions = foldl (updatePosition s) Set.empty (positions s)})
@@ -60,47 +54,48 @@ updatePositions = S.modify (\s -> s {positions = foldl (updatePosition s) Set.em
 updateTime :: S.State State ()
 updateTime = S.modify (\s -> s {time = time s + 1})
 
-updateBlizzard :: Int -> Int -> Blizzard -> Blizzard
-updateBlizzard _ h ((x, y), MoveUp)
-  | y == 0 = ((x, h - 1), MoveUp)
-  | otherwise = ((x, y - 1), MoveUp)
-updateBlizzard _ h ((x, y), MoveDown)
-  | y == h - 1 = ((x, 0), MoveDown)
-  | otherwise = ((x, y + 1), MoveDown)
-updateBlizzard w _ ((x, y), MoveLeft)
-  | x == 0 = ((w - 1, y), MoveLeft)
-  | otherwise = ((x - 1, y), MoveLeft)
-updateBlizzard w _ ((x, y), MoveRight)
-  | x == w - 1 = ((0, y), MoveRight)
-  | otherwise = ((x + 1, y), MoveRight)
-
 updatePosition :: State -> Set.Set Pos -> Pos -> Set.Set Pos
 updatePosition s ps pos = foldr Set.insert ps reachable
   where
     reachable = filter (\p@(x, y) -> p == start s || p == goal s || (0 <= x && x < w && 0 <= y && y < h)) noBlizzard
-    noBlizzard = filter (Maybe.isNothing . (`List.lookup` blizzards s)) adjacent
+    noBlizzard = filter (checkEmpty s) adjacent
     adjacent = map (\(i, j) -> (fst pos + i, snd pos + j)) [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]
     w = width s
     h = height s
+
+checkEmpty :: State -> Pos -> Bool
+checkEmpty s (x, y) = and [right, left, up, down]
+  where
+    right = blizzs `at` (mod (x - m) w, y) /= MoveRight
+    left = blizzs `at` (mod (x + m) w, y) /= MoveLeft
+    down = blizzs `at` (x, mod (y - m) h) /= MoveDown
+    up = blizzs `at` (x, mod (y + m) h) /= MoveUp
+    blizzs = blizzards s
+    w = width s
+    h = height s
+    m = time s
+
+at :: Blizzards -> Pos -> Blizzard
+at blizzs (x, y) = Maybe.fromMaybe None (blizzs Vector.!? y >>= (Vector.!? x))
 
 -- parse input
 
 parseInput :: [Text.Text] -> State
 parseInput input = State {blizzards = blizzs, positions = Set.singleton entry, time = 0, start = entry, goal = exit, width = w, height = h}
   where
-    blizzs = foldl parseRow [] . zip [0 ..] . tail . init $ input
+    blizzs = Vector.fromList . map parseRow . tail . init $ input
     entry = (hole (head input), -1)
     exit = (hole (last input), length input - 2)
     hole = subtract 1 . Maybe.fromJust . Text.findIndex (== '.')
     w = subtract 2 . Text.length . head $ input
     h = length input - 2
 
-parseRow :: Blizzards -> (Int, Text.Text) -> Blizzards
-parseRow blizzs (row, line) = foldl addBlizzard blizzs . zip (zip [-1 ..] (repeat row)) . Text.unpack $ line
+parseRow :: Text.Text -> Vector.Vector Blizzard
+parseRow = Vector.fromList . map parseBlizzard . tail . init . Text.unpack
 
-addBlizzard :: Blizzards -> (Pos, Char) -> Blizzards
-addBlizzard blizzs (pos, '>') = (pos, MoveRight) : blizzs
-addBlizzard blizzs (pos, '<') = (pos, MoveLeft) : blizzs
-addBlizzard blizzs (pos, '^') = (pos, MoveUp) : blizzs
-addBlizzard blizzs (pos, 'v') = (pos, MoveDown) : blizzs
-addBlizzard blizzs _ = blizzs
+parseBlizzard :: Char -> Blizzard
+parseBlizzard '>' = MoveRight
+parseBlizzard '<' = MoveLeft
+parseBlizzard '^' = MoveUp
+parseBlizzard 'v' = MoveDown
+parseBlizzard _ = None
--- test/Day24/BlizzardBasinSpec.hs
+++ test/Day24/BlizzardBasinSpec.hs
@@ -2,7 +2,6 @@
 
 module Day24.BlizzardBasinSpec where
 
-import qualified Data.List as List
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified Day24.BlizzardBasin as B
@@ -29,15 +28,30 @@ spec = do
         B.goal s `shouldBe` (5, 4)
 
       it "should register all blizzards" $ do
-        B.blizzards s `shouldContain` [((0, 0), B.MoveRight)]
-        B.blizzards s `shouldContain` [((5, 1), B.MoveLeft)]
-        B.blizzards s `shouldContain` [((1, 2), B.MoveDown)]
-        B.blizzards s `shouldContain` [((4, 3), B.MoveUp)]
+        B.at (B.blizzards s) (0, 0) `shouldBe` B.MoveRight
+        B.at (B.blizzards s) (5, 1) `shouldBe` B.MoveLeft
+        B.at (B.blizzards s) (1, 2) `shouldBe` B.MoveDown
+        B.at (B.blizzards s) (4, 3) `shouldBe` B.MoveUp
 
-        List.lookup (3, 1) (B.blizzards s) `shouldBe` Nothing
+        B.at (B.blizzards s) (3, 1) `shouldBe` B.None
 
       it "should find the exit" $ do
         B.goForExit s `shouldBe` 18
 
       it "should go back for the snack" $ do
         B.goForSnack s `shouldBe` 54
+
+      it "should check if a position is free at a given time" $ do
+        (0, 0) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (3, 0) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (5, 0) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (1, 1) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (2, 1) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (5, 1) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (2, 2) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (5, 2) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (2, 3) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+        (3, 3) `shouldSatisfy` B.checkEmpty s {B.time = 13}
+
+        (3, 1) `shouldNotSatisfy` B.checkEmpty s {B.time = 13}
+        (5, 3) `shouldNotSatisfy` B.checkEmpty s {B.time = 13}
